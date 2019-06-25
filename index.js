444f09480c7426820bd9f2783887038805749ffc
Roger <idevelopedreams@gmail.com>
2019-06-25 10:57:08 -0400

    initial commit example of importing npm packages

---
+++ index.js
@@ -0,0 +1,6 @@
+const landPunch = require('landmammalpunch');
+const pablo = require('pabloca');
+const isaiah = require('krazymonkey45');
+const noah = require('thebigdog');
+
+noah.greetingHuman()
